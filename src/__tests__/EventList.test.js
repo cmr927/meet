224a304115103545454ea1d0365b2dc857c85c73
cmr927 <142535038+cmr927@users.noreply.github.com>
2024-05-10 18:31:30 -0400

    Wrote tests for user interactions

--- src/__tests__/EventList.test.js
+++ src/__tests__/EventList.test.js
@@ -5,7 +5,7 @@ describe('<EventList /> component', () => {
     let EventListComponent;
     beforeEach(() => {
         EventListComponent = render(<EventList />);
-    })
+    });
 
     test('has an element with "list" role', () => {
         expect(EventListComponent.queryByRole("list")).toBeInTheDocument();
