10bfcdbf9984d4091b66a9920c25e60efe587248
cmr927 <142535038+cmr927@users.noreply.github.com>
2024-06-19 12:16:04 -0400

    `${events}`

--- src/components/CityEventsChart.js
+++ src/components/CityEventsChart.js
@@ -14,7 +14,7 @@ const CityEventsChart = ({ allLocations, events }) => {
 
     useEffect(() => {
         setData(getData());
-    }, [`${data}`]);
+    }, [`${events}`]);
 
     const getData = () => {
         const data = allLocations.map((location) => {
@@ -37,7 +37,7 @@ const CityEventsChart = ({ allLocations, events }) => {
             >
                 <CartesianGrid />
                 <XAxis type="category" dataKey="city" name="City" />
-                <YAxis type="number" dataKey="count" name="Number of events" allowDecimals={false} />
+                <YAxis type="number" dataKey="count" name="Number of events" />
                 <Tooltip cursor={{ strokeDasharray: '3 3' }} />
                 <Scatter name="A school" data={data} fill="#8884d8" />
             </ScatterChart>
