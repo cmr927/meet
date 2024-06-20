4cd8654de80ae426856885f7fc214cc2aa6d6591
cmr927 <142535038+cmr927@users.noreply.github.com>
2024-06-20 15:43:31 -0400

    EventGenresChart updates

--- src/components/EventGenresChart.js
+++ src/components/EventGenresChart.js
@@ -1,21 +1,60 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useState, useEffect, PureComponent } from 'react';
 import { PieChart, Pie, Sector, Cell, ResponsiveContainer } from 'recharts';
 
-//working on the functions below. Based on CityEventsChart, but different
-const EventGenresChart = ({ }) => {
-    const [] = useState([]);
+const EventGenresChart = ({ events }) => {
+    const [data, setData] = useState([]);
+
+    const genres = ['React', 'JavaScript', 'Node', 'jQuery', 'Angular'];
 
     useEffect(() => {
         setData(getData());
     }, [`${events}`]);
 
     const getData = () => {
-        const data = allLocations.map((location) => {
-            const count = events.filter((event) => event.location === location).length
-            const city = location.split((/, | - /))[0]
-            return { city, count };
+        const genreData = genres.map(genre => {
+            const filteredEvents = events.filter(event => event.summary.includes(genre));
+            return {
+                name: genre,
+                value: filteredEvents.length
+            };
         })
-        return data;
+        return genreData;
     };
+
+    const renderCustomizedLabel = ({ cx, cy, midAngle, outerRadius, percent, index }) => {
+        const RADIAN = Math.PI / 180;
+        const radius = outerRadius;
+        const x = cx + radius * Math.cos(-midAngle * RADIAN) * 1.07;
+        const y = cy + radius * Math.sin(-midAngle * RADIAN) * 1.07;
+        return percent ? (
+            <text
+                x={x}
+                y={y}
+                fill="#8884d8"
+                textAnchor={x > cx ? 'start' : 'end'}
+                dominantBaseline="central"
+            >
+                {`${genres[index]} ${(percent * 100).toFixed(0)}%`}
+            </text>
+        ) : null;
+    };
+
+    return (
+        <ResponsiveContainer width="99%" height={400}>
+            <PieChart>
+                <Pie
+                    data={data}
+                    dataKey="value"
+                    fill="#8884d8"
+                    labelLine={false}
+                    label={renderCustomizedLabel}
+                    outerRadius={130}
+                />
+            </PieChart>
+        </ResponsiveContainer>
+    );
 }
+
+
 export default EventGenresChart;
